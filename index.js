b14b51ff208ffbe72ca232b71faaa8048bf5de81
kuhnspokerparties <kuhnspokerparties@gmail.com>
2025-02-24 08:03:57 -0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import BrierLeaderboard from "./BrierLeaderboard";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <BrierLeaderboard />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
